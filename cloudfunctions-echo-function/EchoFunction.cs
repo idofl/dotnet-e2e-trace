ace561ce916b634e217a5be29457c3505104bf76
idofl <ido.flatow@gmail.com>
2021-10-24 17:57:56 +0300

    Refactor Cloud Functions echo service

---
+++ cloudfunctions-echo-function/EchoFunction.cs
@@ -0,0 +1,75 @@
+using Google.Cloud.Functions.Framework;
+using Google.Cloud.Functions.Hosting;
+using Google.Cloud.Diagnostics.Common;
+using Google.Cloud.Diagnostics.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Reflection;
+
+namespace GoogleCloudSamples.EndToEndTracing.Function
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void ConfigureAppConfiguration(WebHostBuilderContext context, IConfigurationBuilder configuration)
+        {
+            configuration.AddEnvironmentVariables(prefix: GoogleCloudOptions.Section);
+        }
+
+        public override void ConfigureServices(WebHostBuilderContext context, IServiceCollection services) 
+        {
+            var googleCloudOptions = new GoogleCloudOptions();
+
+            context.Configuration
+                .GetSection(GoogleCloudOptions.Section)
+                .Bind(googleCloudOptions);
+
+            services.AddGoogleDiagnosticsForAspNetCore(
+                googleCloudOptions.ProjectId,
+                googleCloudOptions.Diagnostics.ServiceName,
+                googleCloudOptions.Diagnostics.Version,
+                TraceOptions.Create(
+                    bufferOptions: BufferOptions.NoBuffer())
+            );
+        }
+    }
+
+    [FunctionsStartup(typeof(Startup))]
+    public class EchoFunction : IHttpFunction
+    {
+        private readonly ILogger<EchoFunction> _logger;
+        private readonly IManagedTracer _tracer;
+
+        public EchoFunction(ILogger<EchoFunction> logger, IManagedTracer tracer) 
+        {
+            _logger = logger;
+            _tracer = tracer;
+        }
+
+        /// <summary>
+        /// Echo function that returns the message it got
+        /// </summary>
+        /// <param name="context">The HTTP context, containing the request and the response.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        public async Task HandleAsync(HttpContext context)
+        {
+            var text = context.Request.Query["message"];
+
+            _logger.LogInformation($"{nameof(HandleAsync)} - Echo function running");
+            _logger.LogInformation($"{nameof(HandleAsync)} - Google TraceID: {_tracer.GetCurrentTraceId()}");
+            _logger.LogInformation($"{nameof(HandleAsync)} - Got message: {text}");
+
+            using (_tracer.StartSpan($"{nameof(EchoFunction)}.{nameof(HandleAsync)}.Processing"))
+            {
+                _logger.LogInformation($"{nameof(HandleAsync)} - Processing...");
+                await Task.Delay(100);
+            }
+            await context.Response.WriteAsync(text);
+        }
+    }
+}
---
+++ cloudfunctions-echo-function/GoogleCloudOptions.cs
@@ -0,0 +1,18 @@
+using Google.Cloud.Diagnostics.AspNetCore;
+
+namespace GoogleCloudSamples.EndToEndTracing.Function
+{
+    public class GoogleCloudOptions
+    {    
+        public const string Section = "GoogleCloud"; 
+        public class DiagnosticsOptions {
+            public string ServiceName { get; set; }
+
+            public string Version { get; set; }
+
+        }
+
+        public string ProjectId { get; set; }
+        public DiagnosticsOptions Diagnostics { get; set; }
+    }
+}
